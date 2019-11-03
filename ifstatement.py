2685fe4493df31b9dea83c12e393d07c79f50e0d
thiha88 <tun88thiha8@gmail.com>
2019-11-03 17:47:54 +0630

    if else statement

---
+++ ifstatement.py
@@ -0,0 +1,80 @@
+# x = int(input("Please enter an integer: "))
+#     #This is user input.
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+# int - integer (Number)
+# str - string (Character)
+# boolean - TRUE, False
+# float - 0.02, 0.5, 0.1
+
+
+#comment
+# x = int(input("Please enter an integer: "))
+
+# if x < 5:
+#     x = 0
+#     print('x is smaller than 5')
+# elif x > 10:
+#     print('x is bigger than 10')
+# elif x == 7:
+#     print('x is equal to 7')
+# elif x > 5 and x < 10:
+#     print('x is between 5 & 10')
+# else:
+#     print('x is undefined')
+
+
+# x = int(input("Please enter your age: "))
+
+# if x > 0 and x <= 18:
+#     print('School')
+# elif x > 18 and x <= 50:
+#     print('Beach')
+# elif x > 50 and x <= 75:
+#     print('Monastery')
+# else:
+#     print('Undefined')
+
+
+# print(''I told that "I don't like it"'')
+
+
+# words = ['cat', 'window', 'defenestrate']
+# for w in words:
+#     print(w, len(w))
+
+# fruits = ['apple', 'banana', 'cucumber', 'pineapple', 'orange']
+# for f in fruits:
+#     print(f, len(f))
+
+
+# for i in range(5):
+#     print(i)
+
+
+# for x in range(0, 100):
+#     print(x)
+
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+
+a = [1, 2, 3, 4, 5]
+for i in range(len(a)):
+    print(i, a[i])
+
+
+# sum(range(4))
+
+# list(range(4))
