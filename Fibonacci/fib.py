4c198015ebeb2570106fa1655464bbafdac2811d
thiha88 <tun88thiha8@gmail.com>
2019-11-10 18:00:03 +0630

    Fabonacci

---
+++ Fibonacci/fib.py
@@ -0,0 +1,21 @@
+import fibo
+
+fibo.fib(500)
+
+f = fibo.fib
+f(100)
+
+fibo.fib(10000)
+
+from fibo import fib, fib2
+
+fib(500)
+
+from fibo import *
+fib(500)
+
+import fibo as fi
+fi.fib(500)
+
+from fibo import fib as fibonacci
+fibonacci(500)
---
+++ Fibonacci/fibo.py
@@ -0,0 +1,19 @@
+def fib(n):
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):
+    """Return a list containing the Fibonacci series"""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+# fib(100)
+# fib2(100)
