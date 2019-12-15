ac8e3634292c09c28c4c5607fd515741390687d5
thiha88 <tun88thiha8@gmail.com>
2019-12-15 17:34:23 +0630

    looping and module

---
+++ student/moduletest.py
@@ -0,0 +1,15 @@
+import mymodule
+
+x = mymodule.student["code"]
+a = mymodule.student["name"]
+y = mymodule.student1["mother"]
+b = mymodule.student1["father"]
+z = mymodule.student2["age"]
+c = mymodule.student2["class"]
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
